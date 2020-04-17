d4900305ad76c20646bd7f68098d57f883e63c08
system-radius <radius.sairo@gmail.com>
2020-04-17 22:42:15 +0800

    Resets the movement of the ball along X-axis back to 0.

--- Ball.lua
+++ Ball.lua
@@ -6,7 +6,7 @@ function Ball:init(x, y, width, height)
   self.width = width
   self.height = height
 
-  self.dx = math.random(2) == 1 and 100 or -100
+  self.dx = 0
   self.dy = math.random(-50, 50)
 end
 
@@ -14,7 +14,7 @@ function Ball:reset()
   self.x = V_WIDTH / 2 - 2
   self.y = V_HEIGHT / 2 - 2
 
-  self.dx = math.random(2) == 1 and 100 or -100
+  self.dx = 0
   self.dy = math.random(-50, 50)
 end
 
